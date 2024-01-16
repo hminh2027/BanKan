d79968eba5b078f42bb19df1e8fdab9a82b86a3c
hminh2027 <dackheros@gmail.com>
2024-01-16 15:41:09 +0700

    feat: init project

---
+++ src/main/java/com/learnjava/BanKan/BanKanApplication.java
@@ -0,0 +1,13 @@
+package com.learnjava.BanKan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanKanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanKanApplication.class, args);
+	}
+
+}
